b03ff509bf1df0d518bbf1e3db07c6b71c18e5aa
AntarSidgi <antarsidgi@gmail.com>
2023-11-28 13:56:05 -0800

    fixing

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-
-x = 0
-for c in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(c - x)), end="")
-    x = 32 if x == 0 else 0
+for i in range(0, 26, 2):
+    print("{:c}{:c}".format(122 - i, (122 - i - 1) - 32), end='')
